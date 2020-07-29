a898cf03c3cfd8ef82540dc4b20172ce7587ef46
slebeaud <simon.lebeaud@insa-rouen.fr>
2020-07-29 09:39:05 +0200

    modif

--- app/src/main/java/com/pilldetectionapp/pilloid/FirstPageActivity.java
+++ app/src/main/java/com/pilldetectionapp/pilloid/FirstPageActivity.java
@@ -32,7 +32,6 @@ public class FirstPageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
         checkPermissions();
